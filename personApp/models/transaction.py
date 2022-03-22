7ed94f708e2b06cb100d428803bafab4090906ab
Saeed <saeed.adelpour99@gmail.com>
2022-03-22 20:20:17 +0430

    remove person_payment

--- personApp/models/transaction.py
+++ personApp/models/transaction.py
@@ -9,9 +9,9 @@ class TransactionManager(models.Manager):
         payment = Payment.objects.create(description=description)
         persons_must_pay = list(filter(lambda _id: _id != paid_person, persons))
 
-        person_payments = []
+        transactions = []
         for person_id in persons_must_pay:
-            person_payments.append(
+            transactions.append(
                 self.model(
                     person_id=person_id,
                     payment=payment,
@@ -21,7 +21,7 @@ class TransactionManager(models.Manager):
             )
 
         pay_value_for_paid_person = -1 * len(persons_must_pay) * price_each
-        person_payments.append(
+        transactions.append(
             self.model(
                 person_id=paid_person,
                 payment=payment,
@@ -29,7 +29,7 @@ class TransactionManager(models.Manager):
                 status=self.model.TransactionStatusChoices.pending,
             )
         )
-        return self.bulk_create(person_payments)
+        return self.bulk_create(transactions)
 
 
 class Transaction(models.Model):
@@ -38,7 +38,7 @@ class Transaction(models.Model):
         paid = "paid", _("Paid")
 
     person = models.ForeignKey(
-        "Person", on_delete=models.CASCADE, related_name="person_payments"
+        "Person", on_delete=models.CASCADE, related_name="transactions"
     )
     payment = models.ForeignKey("Payment", on_delete=models.CASCADE)
     price = models.FloatField()
