941c1268e863eeb415c5cdaf8bab84d5c13fab9a
Saeed <saeed.adelpour99@gmail.com>
2022-03-22 18:56:29 +0430

    define related models

---
+++ person/models/person_payment.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class personPayment(models.Model):
+    class personPaymentStatusChoices(models.TextChoices):
+        pending = "pending", _("Pending")
+        paid = "paid", _("Paid")
+
+    person = models.ForeignKey("Person", on_delete=models.CASCADE)
+    payment = models.ForeignKey("Payment", on_delete=models.CASCADE)
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=personPaymentStatusChoices, max_length=20)
