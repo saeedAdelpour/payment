c1ec2c1e67ad1d43dd4c0e8f98a6ed2cf6eb16c8
Saeed <saeed.adelpour99@gmail.com>
2022-03-22 18:43:18 +0430

    setup db

--- payment/settings/dev.py
+++ payment/settings/dev.py
@@ -4,12 +4,15 @@ from . import *
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", cast=bool)
 
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": config("DATABASE_ENGINE"),
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("DATABASE_HOST"),
+        "PORT": config("DATABASE_PORT"),
     }
 }
-
-
-ALLOWED_HOSTS = []
